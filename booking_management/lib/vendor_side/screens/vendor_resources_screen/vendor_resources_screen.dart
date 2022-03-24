457819f87dd3223a0b14837137e503a5cfcb501a
Rushit <rushit@omtecweb.com>
2022-03-24 15:51:20 +0530

    Vendor Side (Resources Screen)

---
+++ booking_management/lib/vendor_side/screens/vendor_resources_screen/vendor_resources_screen.dart
@@ -0,0 +1,25 @@
+import 'package:booking_management/common_modules/constants/enums.dart';
+import 'package:booking_management/common_modules/custom_appbar/custom_appbar.dart';
+import 'package:booking_management/common_modules/extension_methods/extension_methods.dart';
+import 'package:flutter/material.dart';
+
+import 'vendor_resources_screen_widgets.dart';
+
+class VendorResourcesScreen extends StatelessWidget {
+  const VendorResourcesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CommonAppBarModule(title: "Resources", appBarOption: AppBarOption.singleBackButtonOption),
+
+            Expanded(child: const VendorResourcesListModule().commonAllSidePadding(25)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ booking_management/lib/vendor_side/screens/vendor_resources_screen/vendor_resources_screen_widgets.dart
@@ -0,0 +1,67 @@
+import 'package:booking_management/common_modules/constants/app_images.dart';
+import 'package:booking_management/common_modules/extension_methods/extension_methods.dart';
+import 'package:flutter/material.dart';
+
+import '../../../common_modules/container_decorations.dart';
+
+class VendorResourcesListModule extends StatelessWidget {
+  const VendorResourcesListModule({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 8,
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (context, i){
+        return Container(
+          decoration: shadowDecoration(),
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Row(
+              children: [
+                Container(
+                  height: 65,
+                  width: 65,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                      image: AssetImage(AppImages.vendorImg),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Lorem Ipsum',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Lorem Ipsum is dummy text of the printng.',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ).commonSymmetricPadding(vertical: 5),
+          ),
+        ).commonSymmetricPadding(vertical: 10);
+      },
+    );
+  }
+}

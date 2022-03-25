55a4a56eb2ca05b6f729ab8cbe9a54a7e7582593
jyoti5560 <jyoti@omtecweb.com>
2022-03-25 17:16:09 +0530

    remove unnecessary import

--- booking_management/lib/main.dart
+++ booking_management/lib/main.dart
@@ -1,18 +1,6 @@
-import 'package:booking_management/common_ui/common_screens/forgot_password_screen/forgot_password_screen.dart';
 import 'package:booking_management/common_ui/common_screens/sign_in_screen/sign_in_screen.dart';
-import 'package:booking_management/user_side/screens/book_appointment_screen/book_appointment_screen.dart';
-import 'package:booking_management/user_side/screens/business_details_screen/business_details_screen.dart';
-import 'package:booking_management/user_side/screens/index_screen/index_screen.dart';
-import 'package:booking_management/user_side/screens/user_checkout_screen/user_checkout_screen.dart';
-import 'package:booking_management/user_side/screens/user_map_screen/user_map_screen.dart';
-import 'package:booking_management/user_side/screens/user_profile_screen/user_profile_screen.dart';
-import 'package:booking_management/vendor_side/screens/vendor_profile_screen/vendor_profile_screen.dart';
-import 'package:booking_management/vendor_side/screens/vendor_sign_up_screen/vendor_sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'user_side/screens/user_search_results_screen/user_search_results_screen.dart';
-import 'vendor_side/screens/vendor_settings_screen/vendor_settings_screen.dart';
 
 void main() {
   runApp(const MyApp());

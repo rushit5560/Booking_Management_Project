e67bab204d66e96c288086035f73894883a96129
jyoti5560 <jyoti@omtecweb.com>
2022-05-09 12:59:48 +0530

    Customer(Add sign up API URL)

--- booking_management/lib/common_modules/constants/api_url.dart
+++ booking_management/lib/common_modules/constants/api_url.dart
@@ -1,11 +1,11 @@
 class ApiUrl {
   static const apiMainPath = "http://5.189.147.159:9600/";
-  // Common API
-  static const signInApi = apiMainPath + "Login/Login";
+  /// Common API
+  static const signInApi = apiMainPath + "API/Login";
   static const forgotPasswordApi = apiMainPath + "Login/ForgotPassword";
 
-  // User API
-  static const userSignUpApi = apiMainPath + "Login/CustomerRegister";
+  /// User API
+  static const userSignUpApi = apiMainPath + "API/CustomerRegister";
   static const userBusinessListApi = apiMainPath + "Searching/APIBusinesslist";
   static const userBusinessDetailsApi = apiMainPath + "Searching/APIBusinessDetails";
   static const userBookingHistoryApi = apiMainPath + "Booking/APIBookingHistory";
@@ -14,7 +14,7 @@ class ApiUrl {
   static const userAppointmentListApi = apiMainPath + "CustomerDashboard/APICustomerAppointmentlist";
   static const userAddReviewApi = apiMainPath + "Reviews/APIAddUpdateReview";
 
-  // Vendor API
+  /// Vendor API
   static const vendorSignUpApi = apiMainPath + "API/VendorRegister";
   static const vendorPendingAppointmentApi = apiMainPath + "Appointments/APIAppointmentPending";
   static const vendorAllAppointmentApi = apiMainPath + "Appointments/APIAppointmentGetALL";

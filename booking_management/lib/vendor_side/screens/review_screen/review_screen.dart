2ef074f10ee46132cda01da5e0c2268204fcc1d3
Rushit <rushit@omtecweb.com>
2022-05-13 11:02:41 +0530

    Vendor Side (Review List Screen Done)

--- booking_management/lib/vendor_side/screens/review_screen/review_screen.dart
+++ booking_management/lib/vendor_side/screens/review_screen/review_screen.dart
@@ -3,9 +3,9 @@ import 'package:booking_management/common_modules/custom_appbar/custom_appbar.da
 import 'package:booking_management/common_modules/extension_methods/extension_methods.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../controllers/review_screen_controller/review_screen_controller.dart';
 import 'review_screen_widgets.dart';
+
 
 class ReviewScreen extends StatelessWidget {
   ReviewScreen({Key? key}) : super(key: key);
@@ -17,15 +17,16 @@ class ReviewScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const CommonAppBarModule(title: "Review", appBarOption: AppBarOption.singleBackButtonOption),
-
+            const CommonAppBarModule(
+                title: "Review",
+                appBarOption: AppBarOption.singleBackButtonOption),
             Expanded(
               child: Column(
                 children: [
                   Expanded(
                     child: reviewScreenController.reviewList.isEmpty
-                      ? const Center(child: Text("No Data Available!"))
-                    : const ReviewListModule(),
+                        ? const Center(child: Text("No Data Available!"))
+                        : ReviewListModule(),
                   ),
                 ],
               ).commonSymmetricPadding(horizontal: 15, vertical: 15),

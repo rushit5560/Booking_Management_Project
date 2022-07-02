11748051a9b272c7fad808a33e2e663190d6aba6
chirag0566 <107978521+chirag0566@users.noreply.github.com>
2022-07-02 17:28:43 +0530

    build apk resolved

--- booking_management/lib/user_side/controllers/user_checkout_screen_controller/user_checkout_screen_controller.dart
+++ booking_management/lib/user_side/controllers/user_checkout_screen_controller/user_checkout_screen_controller.dart
@@ -16,7 +16,7 @@ import '../../model/user_checkout_screen_model/checkout_model.dart';
 import '../../model/user_checkout_screen_model/checkout_summary_model.dart';
 import '../../model/user_checkout_screen_model/confirm_checkout_model.dart';
 
-class UserCheckoutScreenController extends GetxController{
+class UserCheckoutScreenController extends GetxController {
   String bookingId = Get.arguments[0];
   String userName = Get.arguments[1];
   String userEmail = Get.arguments[2];
@@ -27,7 +27,7 @@ class UserCheckoutScreenController extends GetxController{
   String secretKey = "";
   String publishableKey = "";
 
-   ApiHeader apiHeader = ApiHeader();
+  ApiHeader apiHeader = ApiHeader();
 
   /// For Stripe
   Map<String, dynamic>? paymentIntentData;
@@ -44,7 +44,7 @@ class UserCheckoutScreenController extends GetxController{
   int bookingPrice = 0;
   int bookingQty = 0;
   int bookingTotalAmount = 0;
-  int transactionId= 0;
+  int transactionId = 0;
 
   GlobalKey<FormState> checkOutFormKey = GlobalKey();
   TextEditingController fNameFieldController = TextEditingController();
@@ -56,31 +56,32 @@ class UserCheckoutScreenController extends GetxController{
 
   RxString payment = 'Credit'.obs;
 
-
-
   /// Checkout
   getCheckoutFunction() async {
     isLoading(true);
     log('bookingId: $bookingId');
-    String url = ApiUrl.customerCheckoutApi + "?id=$bookingId&UserId=${UserDetails.uniqueId}";
+    String url = ApiUrl.customerCheckoutApi +
+        "?id=$bookingId&UserId=${UserDetails.uniqueId}";
     log("Get Checkout API URL : $url");
 
     try {
-      http.Response response = await http.get(Uri.parse(url), /*headers: apiHeader.headers*/);
+      http.Response response = await http.get(
+        Uri.parse(url), /*headers: apiHeader.headers*/
+      );
       log("Checkout Response : ${response.body}");
 
-      CheckoutModel checkoutModel = CheckoutModel.fromJson(json.decode(response.body));
+      CheckoutModel checkoutModel =
+          CheckoutModel.fromJson(json.decode(response.body));
       isSuccessStatus = checkoutModel.success.obs;
 
-      if(isSuccessStatus.value) {
+      if (isSuccessStatus.value) {
         isPriceDisplay = checkoutModel.workerList.isPriceDisplay;
         log("isPriceDisplay : $isPriceDisplay");
       } else {
         //Fluttertoast.showToast(msg: "Something went wrong!");
         log("getCheckoutFunction Else Else");
       }
-
-    } catch(e) {
+    } catch (e) {
       log("getCheckoutFunction Error ::: $e");
     } finally {
       // isLoading(false);
@@ -95,28 +96,36 @@ class UserCheckoutScreenController extends GetxController{
     log("Checkout Summary API URL : $url");
 
     try {
-      http.Response response = await http.get(Uri.parse(url), /*headers: apiHeader.headers*/);
+      http.Response response = await http.get(
+        Uri.parse(url), /*headers: apiHeader.headers*/
+      );
       log("Checkout Summary Response : ${response.body}");
 
-      CheckoutSummaryModel checkoutSummaryModel = CheckoutSummaryModel.fromJson(json.decode(response.body));
+      CheckoutSummaryModel checkoutSummaryModel =
+          CheckoutSummaryModel.fromJson(json.decode(response.body));
       isSuccessStatus = checkoutSummaryModel.success.obs;
 
-      if(isSuccessStatus.value) {
-        vendorName = checkoutSummaryModel.workerList.booking.vendor.businessName;
+      if (isSuccessStatus.value) {
+        vendorName =
+            checkoutSummaryModel.workerList.booking.vendor.businessName;
         vendorImg = checkoutSummaryModel.workerList.booking.vendor.businessLogo;
-        vendorRating = double.parse(checkoutSummaryModel.workerList.booking.vendor.rating.toString());
-        vendorAddress = "${checkoutSummaryModel.workerList.booking.vendor.state}, ${checkoutSummaryModel.workerList.booking.vendor.country}, "
+        vendorRating = double.parse(
+            checkoutSummaryModel.workerList.booking.vendor.rating.toString());
+        vendorAddress =
+            "${checkoutSummaryModel.workerList.booking.vendor.state}, ${checkoutSummaryModel.workerList.booking.vendor.country}, "
             "${checkoutSummaryModel.workerList.booking.vendor.postcode}";
-        String startBookDate = checkoutSummaryModel.workerList.booking.startDateTime;
-        String bDate1 = startBookDate.substring(0, startBookDate.length-9);
+        String startBookDate =
+            checkoutSummaryModel.workerList.booking.startDateTime;
+        String bDate1 = startBookDate.substring(0, startBookDate.length - 9);
         bookingDate = bDate1;
-        String bTime1 = startBookDate.substring(11, startBookDate.length-3);
+        String bTime1 = startBookDate.substring(11, startBookDate.length - 3);
         bookingTime = bTime1;
 
-        String endBookDate = checkoutSummaryModel.workerList.booking.endDateTime;
-        String endDate1 = endBookDate.substring(0, endBookDate.length-23);
+        String endBookDate =
+            checkoutSummaryModel.workerList.booking.endDateTime;
+        String endDate1 = endBookDate.substring(0, endBookDate.length - 23);
         endBookingDate = endDate1;
-        String endTime1 = endBookDate.substring(11, endBookDate.length-17);
+        String endTime1 = endBookDate.substring(11, endBookDate.length - 17);
         endBookingTime = endTime1;
 
         bookingPrice = checkoutSummaryModel.workerList.price.toInt();
@@ -138,8 +147,7 @@ class UserCheckoutScreenController extends GetxController{
         //Fluttertoast.showToast(msg: "Something went wrong!");
         log("getCheckoutSummaryFunction Else Else");
       }
-
-    } catch(e) {
+    } catch (e) {
       log("getCheckoutSummaryFunction Error ::: $e");
     } finally {
       isLoading(false);
@@ -151,7 +159,6 @@ class UserCheckoutScreenController extends GetxController{
       // }
       getStripeKeyFunction();
     }
-
   }
 
   /// get payment Id From payment
@@ -160,10 +167,9 @@ class UserCheckoutScreenController extends GetxController{
     String url = ApiUrl.getPaymentIdApi;
     log("getPaymentId : $url");
 
-
     try {
       var request = http.MultipartRequest('POST', Uri.parse(url));
-       request.headers.addAll(apiHeader.headers);
+      request.headers.addAll(apiHeader.headers);
 
       request.fields['BookingId'] = bookingId;
       request.fields['sessionId'] = secretKey;
@@ -176,20 +182,19 @@ class UserCheckoutScreenController extends GetxController{
       //log('getPaymentId response: ${response.statusCode}');
 
       response.stream.transform(utf8.decoder).listen((value) async {
-        GetPaymentIdModel getPaymentIdModel = GetPaymentIdModel.fromJson(json.decode(value));
+        GetPaymentIdModel getPaymentIdModel =
+            GetPaymentIdModel.fromJson(json.decode(value));
         isSuccessStatus = getPaymentIdModel.success.obs;
         log('isSuccessStatus: $isSuccessStatus');
-        if(isSuccessStatus.value) {
+        if (isSuccessStatus.value) {
           transactionId = getPaymentIdModel.workerList.id;
           log("transactionId : $transactionId");
         } else {
           //Fluttertoast.showToast(msg: "Something went wrong!");
           log("getPaymentId Else Else");
         }
-
       });
-
-    } catch(e) {
+    } catch (e) {
       log("getPaymentId Error ::: $e");
     } finally {
       //isLoading(false);
@@ -222,13 +227,14 @@ class UserCheckoutScreenController extends GetxController{
 
       response.stream.transform(utf8.decoder).listen((value) async {
         log('value: $value');
-        ConfirmCheckoutModel confirmCheckoutModel = ConfirmCheckoutModel.fromJson(json.decode(value));
+        ConfirmCheckoutModel confirmCheckoutModel =
+            ConfirmCheckoutModel.fromJson(json.decode(value));
         isSuccessStatus = confirmCheckoutModel.success.obs;
 
-        if(isSuccessStatus.value) {
+        if (isSuccessStatus.value) {
           returnId = confirmCheckoutModel.id;
           log("returnId : $returnId");
-          Get.to(()=> BookingSuccessScreen(), arguments: returnId);
+          Get.to(() => BookingSuccessScreen(), arguments: returnId);
           // if(returnId.isEmpty){
           //   Get.back();
           // } else{
@@ -239,10 +245,8 @@ class UserCheckoutScreenController extends GetxController{
           //Fluttertoast.showToast(msg: "Something went wrong!");
           log("checkOutSubmitFunction Else Else");
         }
-
       });
-
-    } catch(e) {
+    } catch (e) {
       log("checkOutSubmitFunction Error ::: $e");
     } finally {
       isLoading(false);
@@ -258,62 +262,59 @@ class UserCheckoutScreenController extends GetxController{
 
       await Stripe.instance.initPaymentSheet(
           paymentSheetParameters: SetupPaymentSheetParameters(
-            paymentIntentClientSecret: paymentIntentData!['client_secret'],
-            applePay: true,
-            googlePay: true,
-            // style: ThemeMode.dark,
-            merchantCountryCode: 'US',
-            merchantDisplayName: 'SetDayTime'
-          )
-      );
+        paymentIntentClientSecret: paymentIntentData!['client_secret'],
+        applePay: true,
+        googlePay: true,
+        style: ThemeMode.light,
+        merchantCountryCode: 'US',
+        merchantDisplayName: 'SetDayTime',
+        customFlow: true,
+      ));
 
-      if(paymentIntentData!['id'] == null)
-      {
+      if (paymentIntentData!['id'] == null) {
         log('Failed');
         log('id: ${paymentIntentData!['id']}');
-        Get.snackbar(
-            "Failed", "Failed payment Id", snackPosition: SnackPosition.BOTTOM
-        );
-      } else{
+        Get.snackbar("Failed", "Failed payment Id",
+            snackPosition: SnackPosition.BOTTOM);
+      } else {
         //log('Success');
         log('id: ${paymentIntentData!['id']}');
         // Get.snackbar(
         //     "Success", "Paid Successfully", snackPosition: SnackPosition.BOTTOM
         // );
-         getPaymentIdFunction(paymentIntentData!['id'], paymentIntentData!['client_secret']);
+        getPaymentIdFunction(
+            paymentIntentData!['id'], paymentIntentData!['client_secret']);
       }
 
       //await displayPaymentSheet();
       //await checkOutSubmitFunction();
 
-    } catch(e) {
+    } catch (e) {
       log("Make Payment Error ::: $e");
+      rethrow;
     }
   }
 
   createPaymentIntent(int amount, String currency) async {
     try {
       Map<String, dynamic> body = {
-        "amount" : calculateAmount(amount),
+        "amount": calculateAmount(amount),
         'currency': currency,
         'payment_method_types[]': 'card'
       };
       log('body: $body');
 
       var response = await http.post(
-        Uri.parse('https://api.stripe.com/v1/payment_intents'),
-        body: body,
+          Uri.parse('https://api.stripe.com/v1/payment_intents'),
+          body: body,
           headers: {
             'Authorization': 'Bearer ${PaymentKeys.secretKey}',
             'Content-Type': 'application/x-www-form-urlencoded'
-          }
-      );
+          });
       //log("response.body: ${response.body}");
       //log("response.statusCode: ${response.statusCode}");
       return jsonDecode(response.body.toString());
-
-
-    } catch(e) {
+    } catch (e) {
       log("Create Payment Intent ::: $e");
     }
   }
@@ -326,23 +327,26 @@ class UserCheckoutScreenController extends GetxController{
   displayPaymentSheet() async {
     //isLoading(true);
     try {
-      await Stripe.instance.presentPaymentSheet(
-        parameters: PresentPaymentSheetParameters(
-            clientSecret: paymentIntentData!['client_secret'],
+      await Stripe.instance
+          .presentPaymentSheet(
 
+        parameters: PresentPaymentSheetParameters(
+          clientSecret: paymentIntentData!['client_secret'],
           confirmPayment: true,
+
         ),
-      ).then((value) {
+      )
+          .then((value) async {
         log('paymentIntentData id : ${paymentIntentData!['id']}');
         log('Display paymentIntentData: $paymentIntentData');
 
+
         isLoading(true);
+        await checkOutSubmitFunction();
         paymentIntentData = null;
         isLoading(false);
         log('success');
       });
-
-
 
       //Get.snackbar("Success", "Paid Successfully", snackPosition: SnackPosition.TOP);
       //Get.back();
@@ -354,38 +358,40 @@ class UserCheckoutScreenController extends GetxController{
       /// API Calling
       //checkOutSubmitFunction();
 
-
-    } on StripeException catch(e) {
+    } on StripeException catch (e) {
       log("StripeException Error ::: $e");
       // Get.snackbar(
       //     "Failed", "Failed to pay", snackPosition: SnackPosition.BOTTOM
       // );
-    } catch(e) {
+    } catch (e) {
       log("Display Payment Sheet Error ::: $e");
-      Get.snackbar(
-          "Failed", "Failed to pay", snackPosition: SnackPosition.BOTTOM
-      );
+      Get.snackbar("Failed", "Failed to pay",
+          snackPosition: SnackPosition.BOTTOM);
     } finally {
       //isLoading(false);
-      log('paymentIntentData id : ${paymentIntentData!['id']}');
-      Get.snackbar("Success", "Paid Successfully", snackPosition: SnackPosition.TOP);
-      await checkOutSubmitFunction();
+      // log('paymentIntentData id : ${paymentIntentData!['id']}');
+      Get.snackbar("Success", "Paid Successfully",
+          snackPosition: SnackPosition.TOP);
+      // await checkOutSubmitFunction();
     }
   }
 
-  getStripeKeyFunction() async{
+  getStripeKeyFunction() async {
     isLoading(true);
     String url = ApiUrl.getSecretKeyApi;
     log("Get Stripe Secret Key API URL : $url");
 
     try {
-      http.Response response = await http.get(Uri.parse(url), /*headers: apiHeader.headers*/);
+      http.Response response = await http.get(
+        Uri.parse(url), /*headers: apiHeader.headers*/
+      );
       log("Stripe Secret Key Response : ${response.body}");
 
-      GetStripeSecretKeyModel getStripeSecretKeyModel = GetStripeSecretKeyModel.fromJson(json.decode(response.body));
+      GetStripeSecretKeyModel getStripeSecretKeyModel =
+          GetStripeSecretKeyModel.fromJson(json.decode(response.body));
       isSuccessStatus = getStripeSecretKeyModel.success.obs;
 
-      if(isSuccessStatus.value) {
+      if (isSuccessStatus.value) {
         secretKey = getStripeSecretKeyModel.workerList.secretKey;
         publishableKey = getStripeSecretKeyModel.workerList.publishableKey;
         log("secretKey : $secretKey");
@@ -394,24 +400,22 @@ class UserCheckoutScreenController extends GetxController{
         //Fluttertoast.showToast(msg: "Something went wrong!");
         log("getStripeKeyFunction Else Else");
       }
-
-    } catch(e) {
+    } catch (e) {
       log("getStripeKeyFunction Error ::: $e");
     } finally {
-       isLoading(false);
-
+      isLoading(false);
     }
   }
 
-
-
   @override
   void onInit() {
-
-    if(UserDetails.isUserLoggedIn == true) {
-      fNameFieldController.text = UserDetails.userName.isEmpty ? "" : UserDetails.userName;
-      emailFieldController.text = UserDetails.email.isEmpty ? "" : UserDetails.email;
-      phoneFieldController.text = UserDetails.phoneNo.isEmpty ? "" : UserDetails.phoneNo;
+    if (UserDetails.isUserLoggedIn == true) {
+      fNameFieldController.text =
+          UserDetails.userName.isEmpty ? "" : UserDetails.userName;
+      emailFieldController.text =
+          UserDetails.email.isEmpty ? "" : UserDetails.email;
+      phoneFieldController.text =
+          UserDetails.phoneNo.isEmpty ? "" : UserDetails.phoneNo;
     } else {
       fNameFieldController.text = userName;
       emailFieldController.text = userEmail;
@@ -421,7 +425,4 @@ class UserCheckoutScreenController extends GetxController{
 
     super.onInit();
   }
-
-
-
 }
--- booking_management/lib/vendor_side/controllers/vendor_subscription_plan_screen_controller/vendor_subscription_plan_screen_controller.dart
+++ booking_management/lib/vendor_side/controllers/vendor_subscription_plan_screen_controller/vendor_subscription_plan_screen_controller.dart
@@ -34,23 +34,21 @@ class VendorSubscriptionPlanScreenController extends GetxController {
 
       await Stripe.instance.initPaymentSheet(
           paymentSheetParameters: SetupPaymentSheetParameters(
-              paymentIntentClientSecret: paymentIntentData!['client_secret'],
-              applePay: true,
-              googlePay: true,
-              // style: ThemeMode.dark,
-              merchantCountryCode: 'US',
-              merchantDisplayName: 'SetDayTime'
-          )
-      );
+        paymentIntentClientSecret: paymentIntentData!['client_secret'],
+        applePay: true,
+        googlePay: true,
+        // style: ThemeMode.dark,
+        merchantCountryCode: 'US',
+        merchantDisplayName: 'SetDayTime',
+        customFlow: true,
+      ));
 
-      if(paymentIntentData!['id'] == null)
-      {
+      if (paymentIntentData!['id'] == null) {
         log('Failed');
         log('id: ${paymentIntentData!['id']}');
-        Get.snackbar(
-            "Failed", "Failed payment Id", snackPosition: SnackPosition.BOTTOM
-        );
-      } else{
+        Get.snackbar("Failed", "Failed payment Id",
+            snackPosition: SnackPosition.BOTTOM);
+      } else {
         //log('Success');
         log('id: ${paymentIntentData!['id']}');
         // Get.snackbar(
@@ -63,7 +61,7 @@ class VendorSubscriptionPlanScreenController extends GetxController {
       //Get.back();
       //await checkOutSubmitFunction();
 
-    } catch(e) {
+    } catch (e) {
       log("Make Payment Error ::: $e");
     } finally {
       isLoading(false);
@@ -73,15 +71,17 @@ class VendorSubscriptionPlanScreenController extends GetxController {
   displayPaymentSheet() async {
     isLoading(true);
     try {
-      Stripe.instance.presentPaymentSheet(
+      Stripe.instance
+          .presentPaymentSheet(
         parameters: PresentPaymentSheetParameters(
           clientSecret: paymentIntentData!['client_secret'],
-
           confirmPayment: true,
         ),
-      ).then((value) {
+      )
+          .then((value) {
         log('paymentIntentData id : ${paymentIntentData!['id']}');
-        Get.snackbar("Success", "Paid Successfully", snackPosition: SnackPosition.TOP);
+        Get.snackbar("Success", "Paid Successfully",
+            snackPosition: SnackPosition.TOP);
         log('paymentIntentData id : ${paymentIntentData!['id']}');
         log('Display paymentIntentData: $paymentIntentData');
 
@@ -90,8 +90,6 @@ class VendorSubscriptionPlanScreenController extends GetxController {
         isLoading(false);
         log('success');
       });
-
-
 
       //Get.snackbar("Success", "Paid Successfully", snackPosition: SnackPosition.TOP);
       //Get.back();
@@ -103,17 +101,15 @@ class VendorSubscriptionPlanScreenController extends GetxController {
       /// API Calling
       //checkOutSubmitFunction();
 
-
-    } on StripeException catch(e) {
+    } on StripeException catch (e) {
       log("StripeException Error ::: $e");
       // Get.snackbar(
       //     "Failed", "Failed to pay", snackPosition: SnackPosition.BOTTOM
       // );
-    } catch(e) {
+    } catch (e) {
       log("Display Payment Sheet Error ::: $e");
-      Get.snackbar(
-          "Failed", "Failed to pay", snackPosition: SnackPosition.BOTTOM
-      );
+      Get.snackbar("Failed", "Failed to pay",
+          snackPosition: SnackPosition.BOTTOM);
     } finally {
       isLoading(false);
       // log('paymentIntentData id : ${paymentIntentData!['id']}');
@@ -130,7 +126,7 @@ class VendorSubscriptionPlanScreenController extends GetxController {
   createPaymentIntent(int amount, String currency) async {
     try {
       Map<String, dynamic> body = {
-        "amount" : calculateAmount(amount),
+        "amount": calculateAmount(amount),
         'currency': currency,
         'payment_method_types[]': 'card'
       };
@@ -142,14 +138,11 @@ class VendorSubscriptionPlanScreenController extends GetxController {
           headers: {
             'Authorization': 'Bearer ${PaymentKeys.secretKey}',
             'Content-Type': 'application/x-www-form-urlencoded'
-          }
-      );
+          });
       //log("response.body: ${response.body}");
       //log("response.statusCode: ${response.statusCode}");
       return jsonDecode(response.body.toString());
-
-
-    } catch(e) {
+    } catch (e) {
       log("Create Payment Intent ::: $e");
     }
   }
@@ -182,7 +175,7 @@ class VendorSubscriptionPlanScreenController extends GetxController {
         // ConfirmCheckoutModel confirmCheckoutModel = ConfirmCheckoutModel.fromJson(json.decode(value));
         //isSuccessStatus = confirmCheckoutModel.success.obs;
 
-        if(isSuccessStatus.value) {
+        if (isSuccessStatus.value) {
           //returnId = confirmCheckoutModel.id;
           //log("returnId : $returnId");
           //Get.to(()=> BookingSuccessScreen(), arguments: returnId);
@@ -196,10 +189,8 @@ class VendorSubscriptionPlanScreenController extends GetxController {
           //Fluttertoast.showToast(msg: "Something went wrong!");
           log("checkOutSubmitFunction Else Else");
         }
-
       });
-
-    } catch(e) {
+    } catch (e) {
       log("checkOutSubmitFunction Error ::: $e");
     } finally {
       isLoading(false);
@@ -212,30 +203,27 @@ class VendorSubscriptionPlanScreenController extends GetxController {
     log("Get All Subscription Plan API URL : $url");
 
     try {
-      http.Response response = await http.get(Uri.parse(url), headers: apiHeader.headers);
+      http.Response response =
+          await http.get(Uri.parse(url), headers: apiHeader.headers);
       log("Subscription Plan Response : ${response.body}");
 
-      GetAllSubscriptionModel getAllSubscriptionModel = GetAllSubscriptionModel.fromJson(json.decode(response.body));
+      GetAllSubscriptionModel getAllSubscriptionModel =
+          GetAllSubscriptionModel.fromJson(json.decode(response.body));
       isSuccessStatus = getAllSubscriptionModel.success.obs;
 
-      if(isSuccessStatus.value) {
+      if (isSuccessStatus.value) {
         allSubscriptionList.clear();
         allSubscriptionList = getAllSubscriptionModel.workerList;
 
-        for(int i=0; i< getAllSubscriptionModel.workerList.length; i++){
-
-        }
+        for (int i = 0; i < getAllSubscriptionModel.workerList.length; i++) {}
         log("allSubscriptionList :${allSubscriptionList.length}");
-
       } else {
         log("getAllSubscriptionPlanFunction Else Else");
         Fluttertoast.showToast(msg: "Something went wrong!");
       }
-
-
-    } catch(e) {
+    } catch (e) {
       log("getAllSubscriptionPlanFunction Error ::: $e");
-    }finally {
+    } finally {
       isLoading(false);
     }
   }
@@ -245,5 +233,4 @@ class VendorSubscriptionPlanScreenController extends GetxController {
     getAllSubscriptionPlanFunction();
     super.onInit();
   }
-
 }

65ffbdfc5a0445de1df13f3dfa986203155fc9f8
jyoti5560 <jyoti@omtecweb.com>
2022-07-23 14:39:05 +0530

    get user profile model

--- booking_management/lib/user_side/model/user_profile_screen_model/user_profile_details_model.dart
+++ booking_management/lib/user_side/model/user_profile_screen_model/user_profile_details_model.dart
@@ -35,168 +35,395 @@ class UserProfileDetailsModel {
 class Data {
   Data({
     required this.id,
+    required this.categories,
+    required this.categoryId,
+    required this.businessName,
+    required this.businessLogo,
+    required this.street,
+    required this.suburb,
+    required this.postcode,
+    required this.state,
+    required this.country,
+    required this.userName,
     required this.email,
     required this.phoneNo,
-    required this.gender,
-    required this.userName,
-    required this.dateOfBirth,
+    required this.address,
     required this.isActive,
     required this.userId,
-    // required this.applicationUser,
+    required this.vendorPortal,
+    required this.vendorVerification,
+    required this.businessId,
+    required this.isResource,
+    required this.isPriceDisplay,
+    required this.confirmation,
+    required this.isServiceSlots,
+    required this.latitude,
+    required this.longitude,
+    required this.firstPayment,
+    required this.nextPayment,
+    required this.vendorVerificationDate,
+    //required this.applicationUser,
     required this.modifiedBy,
     required this.modifiedOn,
-    // required this.applicationUserModifier,
+    //required this.applicationUserModifier,
+    required this.review,
+    required this.rating,
+    required this.vendorWorkingHours,
+    required this.status,
+    required this.category,
     required this.passwordHash,
-    required this.notes,
-    required this.resourceId,
-    required this.bookingId,
-    required this.isPriceDisplay,
+    required this.workingHoursStatus,
+    required this.avilableTime,
+    required this.dDate,
     required this.duration,
+    required this.startTime,
+    required this.endTime,
+    required this.vendorList,
+    required this.additionalSlot,
+    required this.resourceList,
+    required this.resourceId,
+    required this.service,
+    required this.resource,
+    required this.termsConditions,
+    required this.fullName,
+    required this.stripeId,
   });
 
   int id;
+  Categories categories;
+  int categoryId;
+  String businessName;
+  String businessLogo;
+  String street;
+  String suburb;
+  String postcode;
+  String state;
+  String country;
+  String userName;
   String email;
   String phoneNo;
-  String gender;
-  String userName;
-  String dateOfBirth;
+  String address;
   bool isActive;
   String userId;
-  // ApplicationUser applicationUser;
+  bool vendorPortal;
+  bool vendorVerification;
+  String businessId;
+  bool isResource;
+  bool isPriceDisplay;
+  bool confirmation;
+  bool isServiceSlots;
+  String latitude;
+  String longitude;
+  String firstPayment;
+  String nextPayment;
+  String vendorVerificationDate;
+  //ApplicationUser applicationUser;
   String modifiedBy;
   String modifiedOn;
-  // ApplicationUser applicationUserModifier;
+  //ApplicationUser applicationUserModifier;
+  String review;
+  String rating;
+  String vendorWorkingHours;
+  String status;
+  String category;
   String passwordHash;
-  String notes;
+  String workingHoursStatus;
+  String avilableTime;
+  String dDate;
+  int duration;
+  String startTime;
+  String endTime;
+  String vendorList;
+  String additionalSlot;
+  String resourceList;
   String resourceId;
-  String bookingId;
-  bool isPriceDisplay;
-  String duration;
+  String service;
+  String resource;
+  bool termsConditions;
+  String fullName;
+  String stripeId;
 
   factory Data.fromJson(Map<String, dynamic> json) => Data(
     id: json["id"] ?? 0,
+    categories: Categories.fromJson(json["categories"] ?? {}),
+    categoryId: json["categoryId"] ?? 0,
+    businessName: json["businessName"] ?? "",
+    businessLogo: json["businessLogo"] ?? "",
+    street: json["street"] ?? "",
+    suburb: json["suburb"] ?? "",
+    postcode: json["postcode"] ?? "",
+    state: json["state"] ?? "",
+    country: json["country"] ?? "",
+    userName: json["userName"] ?? "",
     email: json["email"] ?? "",
     phoneNo: json["phoneNo"] ?? "",
-    gender: json["gender"] ?? "",
-    userName: json["userName"] ?? "",
-    dateOfBirth: json["dateOfBirth"] ?? "",
+    address: json["address"] ?? "",
     isActive: json["isActive"] ?? false,
     userId: json["userId"] ?? "",
-    // applicationUser: ApplicationUser.fromJson(json["applicationUser"]),
+    vendorPortal: json["vendorPortal"] ?? false,
+    vendorVerification: json["vendorVerification"] ?? false,
+    businessId: json["businessId"] ?? "",
+    isResource: json["isResource"] ?? false,
+    isPriceDisplay: json["isPriceDisplay"] ?? false,
+    confirmation: json["confirmation"] ?? false,
+    isServiceSlots: json["isServiceSlots"] ?? false,
+    latitude: json["latitude"] ?? "",
+    longitude: json["longitude"] ?? "",
+    firstPayment: json["firstPayment"] ?? "",
+    nextPayment: json["nextPayment"] ?? "",
+    vendorVerificationDate: json["vendorVerificationDate"] ?? "",
+    //applicationUser: ApplicationUser.fromJson(json["applicationUser"]),
     modifiedBy: json["modifiedBy"] ?? "",
     modifiedOn: json["modifiedOn"] ?? "",
-    // applicationUserModifier: ApplicationUser.fromJson(json["applicationUserModifier"]),
+    //applicationUserModifier: ApplicationUser.fromJson(json["applicationUserModifier"]),
+    review: json["review"] ?? "",
+    rating: json["rating"] ?? "",
+    vendorWorkingHours: json["vendorWorkingHours"] ?? "",
+    status: json["status"] ?? "",
+    category: json["category"] ?? "",
     passwordHash: json["passwordHash"] ?? "",
-    notes: json["notes"] ?? "",
+    workingHoursStatus: json["workingHoursStatus"] ?? "",
+    avilableTime: json["avilableTime"] ?? "",
+    dDate: json["dDate"] ?? "",
+    duration: json["duration"] ?? 0,
+    startTime: json["startTime"] ?? "",
+    endTime: json["endTime"] ?? "",
+    vendorList: json["vendorList"] ??"",
+    additionalSlot: json["additionalSlot"] ?? "",
+    resourceList: json["resourceList"] ?? "",
     resourceId: json["resourceId"] ?? "",
-    bookingId: json["bookingId"] ?? "",
-    isPriceDisplay: json["isPriceDisplay"] ?? false,
-    duration: json["duration"] ?? "",
+    service: json["service"] ?? "",
+    resource: json["resource"] ?? "",
+    termsConditions: json["termsConditions"] ?? false,
+    fullName: json["fullName"] ?? "",
+    stripeId: json["stripeId"] ?? "",
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
+    "categories": categories.toJson(),
+    "categoryId": categoryId,
+    "businessName": businessName,
+    "businessLogo": businessLogo,
+    "street": street,
+    "suburb": suburb,
+    "postcode": postcode,
+    "state": state,
+    "country": country,
+    "userName": userName,
     "email": email,
     "phoneNo": phoneNo,
-    "gender": gender,
-    "userName": userName,
-    "dateOfBirth": dateOfBirth,
+    "address": address,
     "isActive": isActive,
     "userId": userId,
-    // "applicationUser": applicationUser.toJson(),
+    "vendorPortal": vendorPortal,
+    "vendorVerification": vendorVerification,
+    "businessId": businessId,
+    "isResource": isResource,
+    "isPriceDisplay": isPriceDisplay,
+    "confirmation": confirmation,
+    "isServiceSlots": isServiceSlots,
+    "latitude": latitude,
+    "longitude": longitude,
+    "firstPayment": firstPayment,
+    "nextPayment": nextPayment,
+    "vendorVerificationDate": vendorVerificationDate,
+    //"applicationUser": applicationUser.toJson(),
     "modifiedBy": modifiedBy,
     "modifiedOn": modifiedOn,
-    // "applicationUserModifier": applicationUserModifier.toJson(),
+    //"applicationUserModifier": applicationUserModifier.toJson(),
+    "review": review,
+    "rating": rating,
+    "vendorWorkingHours": vendorWorkingHours,
+    "status": status,
+    "category": category,
     "passwordHash": passwordHash,
-    "notes": notes,
-    "resourceId": resourceId,
-    "bookingId": bookingId,
-    "isPriceDisplay": isPriceDisplay,
+    "workingHoursStatus": workingHoursStatus,
+    "avilableTime": avilableTime,
+    "dDate": dDate,
     "duration": duration,
+    "startTime": startTime,
+    "endTime": endTime,
+    "vendorList": vendorList,
+    "additionalSlot": additionalSlot,
+    "resourceList": resourceList,
+    "resourceId": resourceId,
+    "service": service,
+    "resource": resource,
+    "termsConditions": termsConditions,
+    "fullName": fullName,
+    "stripeId": stripeId,
   };
 }
 
-// class Data {
-//   Data({
-//     required this.id,
-//     required this.email,
-//     required this.phoneNo,
-//     required this.gender,
-//     required this.userName,
-//     required this.dateOfBirth,
-//     required this.isActive,
-//     required this.userId,
-//     // required this.applicationUser,
-//     required this.modifiedBy,
-//     required this.modifiedOn,
-//     // required this.applicationUserModifier,
-//     required this.passwordHash,
-//     required this.notes,
-//     required this.resourceId,
-//     required this.bookingId,
-//     required this.isPriceDisplay,
-//     required this.duration,
-//   });
-//
-//   int id;
-//   String email;
-//   String phoneNo;
-//   String gender;
-//   String userName;
-//   DateTime dateOfBirth;
-//   bool isActive;
-//   String userId;
-//   // ApplicationUser applicationUser;
-//   String modifiedBy;
-//   String modifiedOn;
-//   // ApplicationUser applicationUserModifier;
-//   String passwordHash;
-//   String notes;
-//   String resourceId;
-//   String bookingId;
-//   bool isPriceDisplay;
-//   String duration;
-//
-//   factory Data.fromJson(Map<String, dynamic> json) => Data(
-//     id: json["id"] ?? 0,
-//     email: json["email"] ?? "",
-//     phoneNo: json["phoneNo"] ?? "",
-//     gender: json["gender"] ?? "",
-//     userName: json["userName"] ?? "",
-//     dateOfBirth: DateTime(json["dateOfBirth"] ?? DateTime.now()),
-//     isActive: json["isActive"] ?? false,
-//     userId: json["userId"] ?? "",
-//     // applicationUser: ApplicationUser.fromJson(json["applicationUser"]),
-//     modifiedBy: json["modifiedBy"] ?? "",
-//     modifiedOn: json["modifiedOn"] ?? "",
-//     // applicationUserModifier: ApplicationUser.fromJson(json["applicationUserModifier"]),
-//     passwordHash: json["passwordHash"] ?? "",
-//     notes: json["notes"] ?? "",
-//     resourceId: json["resourceId"] ?? "",
-//     bookingId: json["bookingId"] ?? "",
-//     isPriceDisplay: json["isPriceDisplay"] ?? false,
-//     duration: json["duration"] ?? "",
-//   );
-//
-//   Map<String, dynamic> toJson() => {
-//     "id": id,
-//     "email": email,
-//     "phoneNo": phoneNo,
-//     "gender": gender,
-//     "userName": userName,
-//     "dateOfBirth": dateOfBirth.toIso8601String(),
-//     "isActive": isActive,
-//     "userId": userId,
-//     // "applicationUser": applicationUser.toJson(),
-//     "modifiedBy": modifiedBy,
-//     "modifiedOn": modifiedOn,
-//     // "applicationUserModifier": applicationUserModifier.toJson(),
-//     "passwordHash": passwordHash,
-//     "notes": notes,
-//     "resourceId": resourceId,
-//     "bookingId": bookingId,
-//     "isPriceDisplay": isPriceDisplay,
-//     "duration": duration,
-//   };
-// }
+/*class ApplicationUser {
+  ApplicationUser({
+    this.apiToken,
+    this.frogotToken,
+    this.fcmToken,
+    this.termsConditions,
+    this.fullName,
+    this.returnUrl,
+    this.externalLogins,
+    this.id,
+    this.userName,
+    this.normalizedUserName,
+    this.email,
+    this.normalizedEmail,
+    this.emailConfirmed,
+    this.passwordHash,
+    this.securityStamp,
+    this.concurrencyStamp,
+    this.phoneNumber,
+    this.phoneNumberConfirmed,
+    this.twoFactorEnabled,
+    this.lockoutEnd,
+    this.lockoutEnabled,
+    this.accessFailedCount,
+  });
 
+  String apiToken;
+  dynamic frogotToken;
+  String fcmToken;
+  bool termsConditions;
+  String fullName;
+  dynamic returnUrl;
+  dynamic externalLogins;
+  String id;
+  String userName;
+  String normalizedUserName;
+  String email;
+  String normalizedEmail;
+  bool emailConfirmed;
+  String passwordHash;
+  String securityStamp;
+  String concurrencyStamp;
+  String phoneNumber;
+  bool phoneNumberConfirmed;
+  bool twoFactorEnabled;
+  dynamic lockoutEnd;
+  bool lockoutEnabled;
+  int accessFailedCount;
+
+  factory ApplicationUser.fromJson(Map<String, dynamic> json) => ApplicationUser(
+    apiToken: json["apiToken"],
+    frogotToken: json["frogotToken"],
+    fcmToken: json["fcmToken"],
+    termsConditions: json["termsConditions"],
+    fullName: json["fullName"],
+    returnUrl: json["returnUrl"],
+    externalLogins: json["externalLogins"],
+    id: json["id"],
+    userName: json["userName"],
+    normalizedUserName: json["normalizedUserName"],
+    email: json["email"],
+    normalizedEmail: json["normalizedEmail"],
+    emailConfirmed: json["emailConfirmed"],
+    passwordHash: json["passwordHash"],
+    securityStamp: json["securityStamp"],
+    concurrencyStamp: json["concurrencyStamp"],
+    phoneNumber: json["phoneNumber"],
+    phoneNumberConfirmed: json["phoneNumberConfirmed"],
+    twoFactorEnabled: json["twoFactorEnabled"],
+    lockoutEnd: json["lockoutEnd"],
+    lockoutEnabled: json["lockoutEnabled"],
+    accessFailedCount: json["accessFailedCount"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "apiToken": apiToken,
+    "frogotToken": frogotToken,
+    "fcmToken": fcmToken,
+    "termsConditions": termsConditions,
+    "fullName": fullName,
+    "returnUrl": returnUrl,
+    "externalLogins": externalLogins,
+    "id": id,
+    "userName": userName,
+    "normalizedUserName": normalizedUserName,
+    "email": email,
+    "normalizedEmail": normalizedEmail,
+    "emailConfirmed": emailConfirmed,
+    "passwordHash": passwordHash,
+    "securityStamp": securityStamp,
+    "concurrencyStamp": concurrencyStamp,
+    "phoneNumber": phoneNumber,
+    "phoneNumberConfirmed": phoneNumberConfirmed,
+    "twoFactorEnabled": twoFactorEnabled,
+    "lockoutEnd": lockoutEnd,
+    "lockoutEnabled": lockoutEnabled,
+    "accessFailedCount": accessFailedCount,
+  };
+}*/
+
+class Categories {
+  Categories({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.parentId,
+    required this.image,
+    required this.orderBy,
+    required this.isActive,
+    required this.createdBy,
+    required this.createdOn,
+    required this.modifiedBy,
+    required this.modifiedOn,
+    required this.prefix,
+    // required this.applicationUserCreator,
+    // required this.applicationUserModifier,
+    required this.isServiceSlots,
+  });
+
+  int id;
+  String name;
+  String description;
+  int parentId;
+  String image;
+  int orderBy;
+  bool isActive;
+  String createdBy;
+  String createdOn;
+  String modifiedBy;
+  String modifiedOn;
+  String prefix;
+  //dynamic applicationUserCreator;
+  //dynamic applicationUserModifier;
+  bool isServiceSlots;
+
+  factory Categories.fromJson(Map<String, dynamic> json) => Categories(
+    id: json["id"] ?? 0,
+    name: json["name"] ?? "",
+    description: json["description"] ?? "",
+    parentId: json["parentId"] ?? 0,
+    image: json["image"] ?? "",
+    orderBy: json["orderBy"] ?? 0,
+    isActive: json["isActive"] ?? false,
+    createdBy: json["createdBy"] ?? "",
+    createdOn: json["createdOn"] ?? "",
+    modifiedBy: json["modifiedBy"] ?? "",
+    modifiedOn: json["modifiedOn"] ?? "",
+    prefix: json["prefix"] ?? "",
+    // applicationUserCreator: json["applicationUserCreator"],
+    // applicationUserModifier: json["applicationUserModifier"],
+    isServiceSlots: json["isServiceSlots"] ?? false,
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "description": description,
+    "parentId": parentId,
+    "image": image,
+    "orderBy": orderBy,
+    "isActive": isActive,
+    "createdBy": createdBy,
+    "createdOn": createdOn,
+    "modifiedBy": modifiedBy,
+    "modifiedOn": modifiedOn,
+    "prefix": prefix,
+    // "applicationUserCreator": applicationUserCreator,
+    // "applicationUserModifier": applicationUserModifier,
+    "isServiceSlots": isServiceSlots,
+  };
+}
